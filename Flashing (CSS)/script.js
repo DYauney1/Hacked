79049f729829a0a66febc45c78c00e6e439c2788
Elder Yauney <dallin@yauney.net>
2022-05-29 14:01:09 -0600

    removed redundant files

--- Flashing (CSS)/script.js
+++
@@ -1,6 +0,0 @@
-let body = document.querySelector('body')
-let backgroundColors = ["blue", "green", "orange"]
-
-
-body.addEventListener("click", function() {alert("Yield yourselves up unto us, and unite with us and become acquainted with our secret works, and become our brethren that ye may be like unto us—not our slaves, but our brethren and partners of all our substance")})
-window.addEventListener("beforeunload", function(event) {event.returnValue = "Panic!!!"});
